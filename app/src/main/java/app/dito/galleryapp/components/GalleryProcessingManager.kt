807c341386c9439133a9770b877302b645909242
Gorthdzx <krislaksana123@gmail.com>
2024-01-09 14:10:16 +0700

    feat : composable button and image

--- app/src/main/java/app/dito/galleryapp/components/GalleryProcessingManager.kt
+++ app/src/main/java/app/dito/galleryapp/components/GalleryProcessingManager.kt
@@ -1,14 +1,24 @@
 package app.dito.galleryapp.components
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import app.dito.galleryapp.R
 
 @Composable
 fun GalleryAppMain(modifier: Modifier = Modifier) {
@@ -16,29 +26,51 @@ fun GalleryAppMain(modifier: Modifier = Modifier) {
         mutableIntStateOf(1)
     }
 
-    when (imageId) {
-        1 -> Column(
-            modifier.wrapContentSize(),
-        ) {
-            /*  Image(
-                  painter = painterResource(id = R.drawable),
-                  contentDescription = null
-              )*/
-            Row {
+    Column {
+        when (imageId) {
+            1 -> GallerySwitcher(
+                image = R.drawable.dice_1
+            )
+
+            2 -> GallerySwitcher(
+                image = R.drawable.dice_1
+            )
+
+            3 -> GallerySwitcher(
+                image = R.drawable.dice_1
+            )
+        }
+        Spacer(modifier.height(20.dp))
+        Row {
+            Button(onClick = {
+                imageId++
+            }) {
+
+            }
+
+            Spacer(modifier.height(20.dp))
+            Button(onClick = {
+                imageId--
+            }) {
 
             }
         }
+    }
+}
 
-        2 -> Column(
-            modifier.wrapContentSize()
-        ) {
-            Row {
+@Composable
+fun GallerySwitcher(
+    @DrawableRes image: Int,
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Image(
+            modifier = modifier.border(
+                BorderStroke(4.dp, Color.Black)
+            ),
+            painter = painterResource(image),
+            contentDescription = null,
+        )
 
-            }
-        }
-
-        3 -> Column {
-
-        }
     }
 }
