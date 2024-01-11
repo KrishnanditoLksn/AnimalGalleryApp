44987adf953d1d9af9b1794fedb6223724f70290
Gorthdzx <krislaksana123@gmail.com>
2024-01-11 22:25:31 +0700

    refac : preview function in main activity

--- app/src/main/java/app/dito/galleryapp/MainActivity.kt
+++ app/src/main/java/app/dito/galleryapp/MainActivity.kt
@@ -3,11 +3,13 @@ package app.dito.galleryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import app.dito.galleryapp.components.GalleryAppMain
 import app.dito.galleryapp.ui.theme.GalleryAppTheme
@@ -30,12 +32,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GalleryApp(modifier: Modifier = Modifier) {
-    GalleryAppMain(modifier)
+fun GalleryApp() {
+    GalleryAppPreview()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GalleryAppPreview() {
-    GalleryApp()
+    GalleryAppMain(Modifier.background(color = Color.White))
 }
